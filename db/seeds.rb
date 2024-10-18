228cfce8000f0614a8c9d25e8884d533294a10c5
yashvasundhariya <yashvasundhariya.1@gmail.com>
2024-10-18 13:29:14 +0530

    adding reviews count of bed bedroom room etc and show more for description

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,18 @@ user = User.create({
 
 6.times do |i|
 	property = Property.create!(name: "Redisson", 
-		description: "this is property from airbnb",
+		description: "Beautifully located in a small village called Tandi, a 10km serene drive/ride from Jibhi. And just 50 metre walking from the parking area. Once you are in our treehouse , you will get to see of a kind breathtaking view of the mountains.  We cook delicious traditional Himachali food as well as the food that you really want to have it here at our treehouse while enjoying the view.",
 		headline: "Property you must visit",
 		address_1: "130 vijay nagar", address_2: "behind petrol pump",
 		city: "indore",
 		state: "madhya pradesh",
 		country: "india",
-		price: Money.from_amount((10..100).to_a.sample, 'USD'))
+		price: Money.from_amount((10..100).to_a.sample, 'USD'),
+		bedroom_count: (1..5).to_a.sample,
+		guest_count: (1..20).to_a.sample,
+		bed_count: (1..10).to_a.sample,
+		bathroom_count: (1..4).to_a.sample,
+		)
 
 	property.images.attach(io: File.open("db/images/property_#{i+1}.png"), filename: property.name)
 	property.images.attach(io: File.open("db/images/property_7.png"), filename: property.name)
