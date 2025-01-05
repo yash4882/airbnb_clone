0e4a2a2a4ab9624a0d43cfb13f33f5dc2111b8fa
yash vasundhariya <yashvasundhariya.1@gmail.com>
2025-01-05 19:52:03 +0530

    adding pagination using kaminari gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     # before_action :authenticate_user!
     
     def index
-        @properties = Property.all.order(created_at: :desc)
+        @properties = Property.all.order(created_at: :desc).page(params[:page]).per(10)
     end
 end
